24d16b175ab3b7ee8b165eb3e777fc2e0ac643b8
tanvirgh <t.chowdhury@orsolutionsbv.com>
2020-10-03 10:02:11 +0600

    initial commit

---
+++ src/main/java/com/example/websocketdemo/controller/MessageController.java
@@ -0,0 +1,8 @@
+package com.example.websocketdemo.controller;
+
+/**
+ * Creator : Tanvir Chowdhury
+ * Date    : 2020-10-01
+ */
+public class MessageController {
+}
---
+++ src/main/java/com/example/websocketdemo/model/MessageModel.java
@@ -0,0 +1,8 @@
+package com.example.websocketdemo.model;
+
+/**
+ * Creator : Tanvir Chowdhury
+ * Date    : 2020-10-01
+ */
+public class MessageModel {
+}
